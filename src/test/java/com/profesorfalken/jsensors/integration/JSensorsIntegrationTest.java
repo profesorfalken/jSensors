1142c2b07106f419d556508f6ed46f523ecd27eb
ProfesorFalken <profesorfalken.github@gmail.com>
2017-07-30 21:43:11 +0200

    Start testing Real mode also

---
+++ src/test/java/com/profesorfalken/jsensors/integration/JSensorsIntegrationTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2016-2017 Javier Garcia Alonso.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.profesorfalken.jsensors.integration;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Components;
+
+/**
+ *
+ * @author javier
+ */
+public class JSensorsIntegrationTest {
+	public JSensorsIntegrationTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	/*
+	 * Make a real call and check that components are retrieved
+	 */
+	@Test
+	public void testJSensorsRealReturn() {
+		Map<String, String> config = new HashMap<String, String>();
+
+		config.put("testMode", "REAL");
+		
+		Components components = JSensors.get.config(config).components();
+		assertNotNull("Components is null", components);
+
+		assertTrue("Components lists are not initialised",
+				components.cpus != null && components.gpus != null && components.disks != null);
+		
+		assertTrue("At least one component has been found",
+				components.cpus.size() > 0 || components.gpus.size() > 0 && components.disks.size() > 0);
+	}
+
+}
